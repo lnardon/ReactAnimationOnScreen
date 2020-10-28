ad2aa052501b7e104ac912ed350a8b90685eb232
lnardon <lucaslnardon@gmail.com>
2020-10-28 00:44:41 -0300

    ✨ Add function to detect if element is on the screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+function debounce(func: any, wait: number, immediate: boolean) {
+  var timeout: null;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
-const OnScreenComponent: React.FC = ({ children }) => {
-  function isOnScreen(component: any) {
-    let bounding = component.getBoundingClientRect();
-    return (
-      bounding.top >= 0 &&
-      bounding.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight)
-    );
-  }
+function useOnScreen(
+  className: string,
+  classNameAnimation: string,
+  offset: number
+) {
+  window.onload = () => {
+    let elements = [...document.getElementsByClassName(className)];
+    window.onscroll = (e) => {
+      debounce(
+        (() => {
+          elements.forEach((element) => {
+            console.log(
+              element.getBoundingClientRect().top,
+              window.innerHeight
+            );
+            if (
+              element.getBoundingClientRect().top <
+              window.innerHeight - offset
+            ) {
+              element.classList.add(classNameAnimation);
+            }
+          });
+        })(),
+        500,
+        false
+      );
+    };
+  };
+}
 
-  return <div className="">{children}</div>;
-};
-
-export default OnScreenComponent;
+export default useOnScreen;
