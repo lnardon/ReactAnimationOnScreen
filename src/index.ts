9caacd2a1508a89dd5808a0ff32730da53de9eef
lnardon <lucaslnardon@gmail.com>
2020-10-28 01:37:20 -0300

    ✨ Remove react file to typescript add refactored function validation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 function debounce(func: any, wait: number, immediate: boolean) {
-  var timeout: null;
+  var timeout: any;
   return function () {
     var context = this,
       args = arguments;
@@ -17,29 +17,41 @@ function debounce(func: any, wait: number, immediate: boolean) {
 function useOnScreen(
   className: string,
   classNameAnimation: string,
-  offset: number
+  offset = 0,
+  useDebounce?: boolean
 ) {
   window.onload = () => {
     let elements = [...document.getElementsByClassName(className)];
-    window.onscroll = (e) => {
-      debounce(
-        (() => {
-          elements.forEach((element) => {
-            console.log(
-              element.getBoundingClientRect().top,
-              window.innerHeight
-            );
+    window.onscroll = (e: Event) => {
+      if (useDebounce) {
+        debounce(
+          (() => {
+            elements.forEach((element) => {
+              if (element.classList.contains(classNameAnimation)) {
+                if (
+                  element.getBoundingClientRect().top <
+                  window.innerHeight - offset
+                ) {
+                  element.classList.add(classNameAnimation);
+                }
+              }
+            });
+          })(),
+          500,
+          false
+        );
+      } else {
+        elements.forEach((element) => {
+          if (element.classList.contains(classNameAnimation)) {
             if (
               element.getBoundingClientRect().top <
               window.innerHeight - offset
             ) {
               element.classList.add(classNameAnimation);
             }
-          });
-        })(),
-        500,
-        false
-      );
+          }
+        });
+      }
     };
   };
 }
