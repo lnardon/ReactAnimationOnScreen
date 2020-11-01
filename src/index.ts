1c711951da609518b3a88151fbf930b3c7945a05
lnardon <lucaslnardon@gmail.com>
2020-11-01 18:41:44 -0300

    ✨ Add window onload elements check

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,39 @@ function useOnScreen(
   useDebounce = true
 ) {
   window.onload = () => {
-    window.onscroll = (e: Event) => {
+    for (let i = 0; i < classNames.length; i++) {
+      let elements = [...document.getElementsByClassName(classNames[i])];
+      if (useDebounce) {
+        debounce(
+          (() => {
+            elements.forEach((element) => {
+              if (!element.classList.contains(animationClassNames[i])) {
+                if (
+                  element.getBoundingClientRect().top <
+                  window.innerHeight - offset
+                ) {
+                  element.classList.add(animationClassNames[i]);
+                }
+              }
+            });
+          })(),
+          500,
+          false
+        );
+      } else {
+        elements.forEach((element) => {
+          if (!element.classList.contains(animationClassNames[i])) {
+            if (
+              element.getBoundingClientRect().top <
+              window.innerHeight - offset
+            ) {
+              element.classList.add(animationClassNames[i]);
+            }
+          }
+        });
+      }
+    }
+    window.onscroll = () => {
       for (let i = 0; i < classNames.length; i++) {
         let elements = [...document.getElementsByClassName(classNames[i])];
         if (useDebounce) {
