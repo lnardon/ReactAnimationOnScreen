4e907d67fae0ae901b475bee6d70b66409c6d8a5
lnardon <lucaslnardon@gmail.com>
2020-11-01 19:26:04 -0300

    ✨ Remove debounce function and update README

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 React Animation On Screen is a custom React hook to check when an element is appearing on the screen and apply some animation to it.
 
+[![NPM](https://img.shields.io/npm/v/reactanimationonscreen.svg)](https://www.npmjs.com/package/reactanimationonscreen) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
 ## Installation
 
 ```bash
@@ -10,13 +12,13 @@ npm install reactanimationonscreen
 
 ## Usage
 
-Initiate the function inside an useEffect passing an array of classnames you want to apply the animation, an array of css classes with the animations to be applied, the y offset amount to apply the animation and if you want to use a debounce function to improve performance.
+Initiate the function inside an useEffect passing an array of classnames you want to apply the animation, an array of css classes with the animations to be applied and the y offset amount to apply the animation.
 
 ```tsx
 import useOnScreen from "reactanimationonscreen";
 
 useEffect(() => {
-  const sections = useOnScreen(targetClassNames: string[], animationClassNames: string[], yOffset : number(optional), useDebounce:boolean(optional));
+  const sections = useOnScreen(targetClassNames: string[], animationClassNames: string[], yOffset : number(optional));
 }, []);
 ```
 
@@ -29,8 +31,7 @@ useEffect(() => {
   const sections = useOnScreen(
     ["subtitle", "paragraph"],
     ["subtitleAnimation", "paragraphAnimation"],
-    0,
-    true
+    0
   );
 }, []);
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,25 @@
-function debounce(this: void, func: any, wait: number, immediate: boolean) {
-  let timeout: any;
-  return () => {
-    let context = this;
-    let args = arguments;
-    let later = () => {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    let callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-}
-
 function useOnScreen(
   classNames: string[],
   animationClassNames: string[],
-  offset = 0,
-  useDebounce = true
+  offset = 0
 ) {
   window.onload = () => {
     for (let i = 0; i < classNames.length; i++) {
       let elements = [...document.getElementsByClassName(classNames[i])];
-      if (useDebounce) {
-        debounce(
-          (() => {
-            elements.forEach((element) => {
-              if (!element.classList.contains(animationClassNames[i])) {
-                if (
-                  element.getBoundingClientRect().top <
-                  window.innerHeight - offset
-                ) {
-                  element.classList.add(animationClassNames[i]);
-                }
-              }
-            });
-          })(),
-          500,
-          false
-        );
-      } else {
+      elements.forEach((element) => {
+        if (!element.classList.contains(animationClassNames[i])) {
+          if (
+            element.getBoundingClientRect().top <
+            window.innerHeight - offset
+          ) {
+            element.classList.add(animationClassNames[i]);
+          }
+        }
+      });
+    }
+    window.onscroll = () => {
+      for (let i = 0; i < classNames.length; i++) {
+        let elements = [...document.getElementsByClassName(classNames[i])];
         elements.forEach((element) => {
           if (!element.classList.contains(animationClassNames[i])) {
             if (
@@ -51,40 +30,6 @@ function useOnScreen(
             }
           }
         });
-      }
-    }
-    window.onscroll = () => {
-      for (let i = 0; i < classNames.length; i++) {
-        let elements = [...document.getElementsByClassName(classNames[i])];
-        if (useDebounce) {
-          debounce(
-            (() => {
-              elements.forEach((element) => {
-                if (!element.classList.contains(animationClassNames[i])) {
-                  if (
-                    element.getBoundingClientRect().top <
-                    window.innerHeight - offset
-                  ) {
-                    element.classList.add(animationClassNames[i]);
-                  }
-                }
-              });
-            })(),
-            500,
-            false
-          );
-        } else {
-          elements.forEach((element) => {
-            if (!element.classList.contains(animationClassNames[i])) {
-              if (
-                element.getBoundingClientRect().top <
-                window.innerHeight - offset
-              ) {
-                element.classList.add(animationClassNames[i]);
-              }
-            }
-          });
-        }
       }
     };
   };
